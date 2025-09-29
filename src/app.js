695a81e604cd99947e56805a2782da95d6f69d5e
Shrawani Bali <shrawanib002@gmail.com>
2025-09-29 16:30:30 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,5 +13,16 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+
+// Routes import
+
+import userRoutes from './routes/user.routes.js'
+
+// Routes Declaration 
+
+app.use("/api/v1/users", userRoutes)
   
+
+
 export { app }; 
